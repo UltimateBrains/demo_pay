7418d29948d131f422e26e168c68e22732b7c13f
Amitkumar <kumbharamit8@gmail.com>
2020-01-17 16:13:30 +0530

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,11 +1,11 @@
 <?php
 
 /* Paypal App Client Id */
-define('PAYPAL_CLIENT_ID', 'AY9Y8KltYZBzZFdnavLv83Z81ZxtW_p7SmEeYxxmljiUb0gHdK-YUYX7s3bNRqMffCDN0oSs3VN8XjrK');
+define('PAYPAL_CLIENT_ID', '');
 
 
 /* Paypal App Client Secret */
-define('PAYPAL_CLIENT_SECRET', 'ELVL2xj1Mf--UGLtuD9v9q39DYisn9R9FCLJSagEz-C8gxGdozogmQouupUnQ3bwNnhN-YGxl-6bkreh');
+define('PAYPAL_CLIENT_SECRET', '');
                                 
 
 /* Paypal App Redirect Url */
